ce98473cc646bca6137792a5792f8f56b7493dd4
AGGARWAL'S <AGGARWAL'S@DESKTOP-Q4JVAI0>
2016-12-02 16:28:14 +0530

    code-radar

---
+++ NSIT Connect/Views/CodeRadarPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ExpandableRowListView;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NSIT_Connect.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CodeRadarPage : Page, INotifyPropertyChanged
+    {
+        public CodeRadarPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private ObservableCollection<ExpandableRowListViewControlItem> _Items = new ObservableCollection<ExpandableRowListViewControlItem>();
+        public ObservableCollection<ExpandableRowListViewControlItem> Items
+        {
+            get { return _Items; }
+            set { _Items = value; OnPropertyChanged(nameof(Items)); }
+        }
+    }
+}
